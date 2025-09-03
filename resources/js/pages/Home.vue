c01b069fce85ae577d33e98861cd28d0bcccddb2
Tri Lecao <tri.lecao@MacMini4>
2025-09-03 11:33:39 +0700

    Update

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -6,9 +6,14 @@ import Landing from "./Partials/Landing.vue";
 import Introduction from "./Partials/Introduction.vue";
 
 const logoState = ref("hidden");
-// states: "hidden", "white", "black"
+const isLoaded = ref(false);
+const progress = ref(0);
 
 onMounted(() => {
+  preloadImages().then(() => {
+    isLoaded.value = true;
+  });
+
   const options = {
     threshold: 0.2,
   };
@@ -36,9 +41,59 @@ onMounted(() => {
 
   onBeforeUnmount(() => observer.disconnect());
 });
+
+// Preload Site
+const frameCount = 300;
+const currentFrame = (i) =>
+  `/images/pawnbg/PawnComp_${String(i).padStart(5, "0")}.jpg`;
+
+function preloadImages() {
+  return new Promise((resolve) => {
+    let loaded = 0;
+    for (let i = 0; i < frameCount; i++) {
+      const img = new Image();
+      img.src = currentFrame(i);
+      const updateProgress = () => {
+        loaded++;
+        let percent = Math.round((loaded / frameCount) * 100);
+
+        // Snap to nearest multiple of 5
+        progress.value = Math.min(100, Math.ceil(percent / 5) * 5);
+
+        if (loaded === frameCount) {
+          progress.value = 100; // make sure we end exactly at 100%
+          resolve();
+        }
+      };
+
+      img.onload = updateProgress;
+      img.onerror = updateProgress;
+    }
+  });
+}
 </script>
 <template>
   <Head title="Welcome" />
+
+  <!-- Loader Screen -->
+  <transition name="fade">
+    <div
+      v-if="!isLoaded"
+      class="fixed inset-0 z-50 flex flex-col items-center justify-center bg-[#FEFBF6]"
+    >
+      <div>
+        <p class="tracking-tight text-2xl sm:text-3xl xl:text-5xl">
+          <b class="font-extrabold">Metanoia</b> Chương III
+        </p>
+        <span
+          class="text-3xl sm:text-4xl xl:text-6xl block font-display rotate-[-4deg] mx-auto text-center text-blue-700"
+        >
+          authenticity
+        </span>
+      </div>
+      <div class="sm:mt-6 text-[6rem] sm:text-[6rem] xl:text-[8rem] font-bold opacity-90 tracking-tighter wave-text">{{ progress }}%</div>
+    </div>
+  </transition>
   
   <div class="w-full min-h-screen">
     <!-- Floating Logo -->
@@ -120,3 +175,34 @@ onMounted(() => {
     </section>
   </div>
 </template>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.8s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.wave-text {
+  background: linear-gradient(90deg, #CB0404 20%, #111111 40%, #CB0404 100%);
+  background-size: 200% 100%;
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  animation: waveText 5s linear infinite;
+  white-space: nowrap;
+  display: inline-block;
+}
+
+@keyframes waveText {
+  0% {
+    background-position: 200% 0;
+  }
+  100% {
+    background-position: -200% 0;
+  }
+}
+</style>
