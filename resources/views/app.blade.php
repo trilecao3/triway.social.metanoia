e9e32b4cd625ca560969d509ef26ee9ab4793e27
Tri Lecao <tri.lecao@MacMini4>
2025-09-04 14:52:24 +0700

    Update favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>{{ config('app.name', 'Metanoia III') }}</title>
 
-        <link rel="icon" href="/favicons/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicons/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+        <link rel="manifest" href="/favicons/site.webmanifest">
 
         @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         @inertiaHead
