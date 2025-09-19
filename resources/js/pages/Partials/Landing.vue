7a16225363811d4117a33219161bd1ae2aeb8b44
Tri Lecao <tri.lecao@MacMini4>
2025-09-19 16:10:35 +0700

    Upload logo

--- resources/js/pages/Partials/Landing.vue
+++ resources/js/pages/Partials/Landing.vue
@@ -36,42 +36,42 @@ const opacities = ref(loopWords.map(() => randomOpacity()))
 </script>
 <template>
   <div
-    class="relative flex h-screen w-full bg-black/97 text-white"
+    class="relative flex h-screen w-full bg-gradient-to-br from-zinc-900 to-black/85 text-white"
   >
-    <!-- <Carousel class="absolute z-30 top-0 w-full overflow-hidden bg-transparent sm:-mt-5" :show="`row1`" /> -->
-
-    <div class="-mt-[120px] w-full sm:max-w-4xl xl:max-w-7xl z-10 px-4 mx-auto flex flex-col sm:flex-row sm:gap-x-10 xl:gap-x-16 items-center justify-center sm:justify-between">
-      <div class="-mt-10 sm:mt-0 w-full sm:w-1/2 text-5xl font-bold">
-        <img src="/images/window-of-metanoia.png" alt="Metanoia Logo" class="w-[80%] mx-auto sm:w-[90%]" />
-      </div>
-      <div class="w-full sm:w-1/2">
+    <div class="absolute top-8 w-full flex flex-col justify-center items-center gap-y-0.5 sm:gap-y-1 text-sm sm:text-base">
+      <p>Thiết kế và tổ chức bởi</p>
+      <p class="uppercase font-bold">The Tri Way x Triway Social</p>
+    </div>
+    <div class="-mt-[20px] sm:-mt-[50px] w-full sm:max-w-4xl xl:max-w-7xl z-10 px-4 mx-auto flex flex-col sm:gap-x-10 xl:gap-x-16 items-center justify-center">
+      <div class="w-full flex justify-center">
         <div class="-mt-8 sm:-mt-5">
-          <div class="flex items-center justify-center sm:justify-start gap-x-3 sm:gap-x-7 opacity-75">
-            <h1 class="text-[4.5rem] sm:text-[7rem] xl:text-[9rem] font-extrabold tracking-tight">
+          <div class="w-full flex items-center justify-center gap-x-5 sm:gap-x-7 opacity-70 sm:opacity-60">
+            <h1 class="text-[7rem] sm:text-[11rem] xl:text-[14rem] font-extrabold tracking-tight">
               <span v-if="daysUntilTarget.length === 2">
                 {{ daysUntilTarget[0] }}
               </span>
-              <span class="bg-gradient-to-r from-gray-50 to-gray-400/50 bg-clip-text text-transparent">
+              <span class="bg-gradient-to-r from-gray-50 to-gray-400/30 bg-clip-text text-transparent">
                 {{ daysUntilTarget.length === 2 ? daysUntilTarget[1] : daysUntilTarget[0] }}
               </span>
             </h1>
-            <span class="text-[1.75rem] sm:text-[3rem] xl:text-[3.8rem] font-bold leading-9 sm:leading-13 xl:leading-17 tracking-tight -mt-1 sm:-mt-1.5">
+            <span class="text-[2.75rem] sm:text-[4.4rem] xl:text-[5.5rem] font-bold leading-13 sm:leading-20 xl:leading-26 tracking-tight -mt-1 sm:-mt-2 xl:-mt-3">
               ngày<br>
-              <span class="bg-gradient-to-r from-gray-50 to-gray-300/50 bg-clip-text text-transparent">đến với</span>
+              <span class="bg-gradient-to-r from-gray-50 to-gray-300/40 bg-clip-text text-transparent">đến với</span>
             </span>
           </div>
-          <div class="mx-auto sm:mx-0 -mt-2 sm:mt-0 block w-fit">
-            <h1 class="text-xl sm:text-4xl xl:text-5xl tracking-tight"><span class="font-bold">Metanoia</span> Chương III</h1>
-            <span class="block -mt-1 sm:mt-0 font-display text-2xl sm:text-5xl xl:text-6xl text-center rotate-[-4deg] mx-auto">authenticity</span>
+          <div class="mx-auto sm:mx-0 mt-0 block w-full text-center">
+            <h1 class="text-3xl sm:text-4xl xl:text-5xl tracking-tight"><span class="font-bold">Metanoia</span> Chương III</h1>
+            <span class="block mt-0 font-display text-4xl sm:text-5xl xl:text-6xl text-center rotate-[-4deg] mx-auto text-red-500">authenticity</span>
           </div>
-          <div class="mt-9 sm:mt-20 text-center sm:text-left">
-            <p class="text-sm sm:text-base font-medium">Thiết kế & tổ chức bởi</p>
-            <img src="/images/logo/tws-logo.png" alt="Triway Social Logo" class="inline-block w-16 sm:w-24 mt-2" />
-            <!-- <p>Thiết kế & tổ chức bởi</p>
-            <p class="uppercase">
-              <b>The Tri Way</b> x <b>Triway Social Team</b>
-            </p> -->
-          </div>
+        </div>
+      </div>
+      <div class="w-full mt-20 sm:mt-32 space-y-6">
+        <div class="mt-3 flex flex-wrap items-center justify-center gap-x-6 gap-y-5 sm:gap-x-12 sm:gap-y-6">
+          <img src="/images/logo/tws-logo.png" alt="Triway Social Logo" class="w-16 sm:w-24 xl:w-28" />
+          <img src="/images/logo/tgc-logo.png" alt="Triway Social Logo" class="h-8 sm:h-11 xl:h-13" />
+          <img src="/images/logo/mh-logo.png" alt="Triway Social Logo" class="h-10 sm:h-14 xl:h-16" />
+          <img src="/images/logo/fonos-logo.png" alt="Triway Social Logo" class="h-8 sm:h-10 xl:h-12" />
+          <img src="/images/logo/gaonau-logo.png" alt="Triway Social Logo" class="h-9 sm:h-12 xl:h-13" />
         </div>
       </div>
     </div>
